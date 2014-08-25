6d20d24f113c4bf820ccb582c4daca68c90d4ddd
Gapti <markpollard@Marks-iMac.local>
2014-08-25 19:34:56 +0100

    Updating to cater for different size items

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Direction
+{
+	None,
+	North,
+	South,
+	East,
+	West
+}
+
+public class Item : MonoBehaviour  {
+
+	public int id;
+	public int CellWidth;
+	public int CellHeight;
+	public string ItemName;
+	public string ItemDesc;
+	public Direction direction = Direction.None;
+}
